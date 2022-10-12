f9e561b2cfe9ab8459cf393b44f8f0018430679d
Phil.Xing <phil.xing@adlinktech.com>
2022-10-12 11:17:13 +0800

    Update scripts.

--- setup.sh
+++ setup.sh
@@ -3,15 +3,13 @@
 # connect the JTAG and power on the target platform before start OpenOCD
 #
 sudo apt install -y libtool libusb-dev libusb-1.0-0-dev
-git clone https://github.com/ADLINK/ampere-openocd.git
+git clone -b adlink https://github.com/ADLINK/ampere-openocd.git
 if [ "eval $(ssh -T git@github.com-adlink | grep -q "authenticated")" != "" ] ; then
   echo "==========================================================================="
   echo "replace HTTPS access with SSH access if authenticated"
   echo "==========================================================================="
   git remote set-url origin git@github.com-adlink:ADLINK/ampere-openocd.git
 fi
-git checkout adlink
-git submodule update --init --recursive
 cd ampere-openocd
 ./bootstrap
 ./configure --prefix=/home/pxing/ampere-openocd/install_IM/ --enable-ftdi --enable-ft2232_ftd2xx #--enable-armjtagew 
